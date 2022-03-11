38114c323bc56ae79dc273b975734b3306f074cf
Admin <ghostminerx@gmail.com>
2022-03-11 12:04:02 +0700

    some ?

--- upprpo/wsgi.py
+++ upprpo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for opprpo project.
+WSGI config for upprpo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opprpo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'upprpo.settings')
 
 application = get_wsgi_application()
