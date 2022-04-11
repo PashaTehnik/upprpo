c6829b87ae8558f25f12b54af811639df262eea8
Pavel <polovinko.p0112@gmail.com>
2022-04-11 20:45:49 +0700

    fix view images in chats

--- chat/models.py
+++ chat/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from django_extensions.db.models import TimeStampedModel
-from taggit.managers import TaggableManager
+# from django_extensions.db.models import TimeStampedModel
+# from taggit.managers import TaggableManager
 
 
 class Message(models.Model):
@@ -21,6 +21,10 @@ class Chat(models.Model):
     type = models.CharField(max_length=300, choices=CHOICES, default='dialog')
     members = models.ManyToManyField(User)
     message_set = models.ManyToManyField(Message)
+
+
+class Photo(models.Model):
+    file = models.ImageField(upload_to='chat_images/', blank=True)
 
 
 class Photo(models.Model):
