e715090e1ab61ee84e37926d5f15ed944ab72a99
Admin <ghostminerx@gmail.com>
2022-06-22 22:12:25 +0700

    2 unit test

--- chat/tests.py
+++ chat/tests.py
@@ -1,24 +1,23 @@
 from django.test import TestCase
-from chat.models import MessageSerializer, Chat, Message
-from django.contrib.auth import authenticate
-from django.utils import timezone
-from datetime import datetime
+from chat.models import Message
 from django.contrib.auth.models import User
 
 
-class MessageTest(TestCase):
-    def setUP(self):
-        time = timezone.now()
-        User.objects.create("adm")
-        user = authenticate("adm")
-        Message.objects.create(text="Testing", author=user, pub_date=time)
+class MessageTestCase(TestCase):
+
+    def setUp(self):
+        #User.objects.create_user(username="admino")
+        u, created = User.objects.get_or_create(username="admino")
+        user = User.objects.all().last()
+        Message.objects.create(text="Testing", author=user)
 
     def test_message_created(self):
-        #author = Message.objects.all().last().author
-        #text = Message.objects.all().last().text
+        author = Message.objects.all().last().author
+        text = Message.objects.all().last().text
+        u, created = User.objects.get_or_create(username="admino")
         print(User.objects.all())
-        self.assertIsNotNone(Message.objects.all().last())
-        #self.assertEqual(text, 'Testing')
-        #self.assertEqual(author.username, 'admin')
+        self.assertIsNotNone(User.objects.all().last())
+        self.assertEqual(text, 'Testing')
+        self.assertEqual(author.username, 'admino')
 
-# Create your tests here.
+
